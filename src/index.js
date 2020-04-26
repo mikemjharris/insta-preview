57a5700ee31f3029a73cb5c2c09e5e48412ac58c
Mike Harris <hello@mikemjharris.com>
2020-04-26 07:33:42 +0100

    update copy to clipboard

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('Setup');
 
 import './main.scss'
 
+document.getElementById('inputLink').value = "https://www.instagram.com/p/B_QAuXKHlJr/"
 document.getElementById('preview').onclick = () => {
 
   const inputLink = document.getElementById('inputLink').value;
@@ -18,9 +19,39 @@ document.getElementById('preview').onclick = () => {
   .then((data) => data.json())
   .then((data) => {
     document.getElementById('image').src = data.data.image;
-    document.getElementById('title').innerHTML = data.data.description;
+    document.getElementById('title').innerHTML = data.data.title;
+    document.getElementById('decription').innerHTML = data.data.decription;
     const link = window.location.href + instaPath;
     document.getElementById('link').innerHTML = link;
 
   })
 };
+
+
+document.getElementById('copy').onclick = () => {
+
+  const inputLink = document.getElementById('inputLink').value;
+
+  const matches = inputLink.match(/(https:\/\/)?instagram.com(\/p\/[0-9A-z]+)/)
+
+  const url =`http://instagram.mikemjharris.com${matches[2]}`;
+  document.getElementById('url').value = url;
+  new ClipboardJS('#copyLink');
+};
+
+function myFunction() {
+  /* Get the text field */
+    var copyText = document.getElementById("url");
+
+  /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+      document.execCommand("copy");
+
+        /* Alert the copied text */
+          alert("Copied the text: " + copyText.value);
+          }
+
+
